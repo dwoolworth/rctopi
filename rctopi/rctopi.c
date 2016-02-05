0db36202315010d3831ac761d758089c9743b330
Derrick Woolworth <dwoolworth@gmail.com>
2016-02-04 19:16:35 -0600

    first revision of rctopi.c

--- rctopi/rctopi.c
+++ rctopi/rctopi.c
@@ -5,32 +5,42 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define BLUE_LED   0b00000010
-#define RED_LED    0b00000100
-#define YELLOW_LED 0b00001000
+#define CAMERA_OFF          0b00000000 // sw1 down
+#define PICTURE_MODE        0b00000100 // sw1 mid, sw2 mid
+#define VIDEO_MODE          0b00001000 // sw1 high, sw2 mid
 
-volatile int16_t rcpulse = 0;
-volatile int16_t tmprcpulse = 0;
-volatile int16_t rcpulse_start = 0;
+#define PIC_BURST_ACTION    0b00001100 // sw1 mid, sw2 down (picture mode, sw2 down)
+#define PIC_TOGGLE_ACTION   0b00010000 // sw1 mid,  sw2 high
+#define VID_TOGGLE_ACTION   0b00010100 // sw1 high, sw2 high 
+#define SHUTTER_ACTION      0b00011000 // sw1 high, sw2 low (video mode snapshot)
 
-volatile uint16_t secs_to_flash = 1;
+#define AUX1_ACTION         0b00011100
 
-void wait_sec(int seconds);
+#define LOW_PWM  1200
+#define MID_PWM  1800
+#define HIGH_PWM 2200
+
+volatile uint8_t previouspins = 0xFF;
+
+volatile int16_t pwm_p0  = 0;
+volatile int16_t tpwm_p0 = 0;
+volatile int16_t pwm_p1  = 0;
+volatile int16_t tpwm_p1 = 0;
+
 
 int main(void)
 {
-    uint8_t led = BLUE_LED;
+    // enables input on pin0 and pin1, output on pin2, pin3, and pin4
+    DDRB = 0b00011100;
 
-    // enables input on pin0,  output on pin1, pin2, pin3, and pin4
-    DDRB = 0b00011110;
-
-    // set input pins to pull-up
-    PORTB = 0b00000001; // pull-up pins 0 and 1
+    // set input pins with pull-up enabled
+    PORTB |= ((1 << PORTB0) | (1 << PORTB1));
 
     // set PCIE to enable PCMSK scan
     GIMSK |= (1 << PCIE);
 
-    // set PCINT0 to trigger an interrupt on state change
+    // this enables the pin change interrupt on state change on PCINT0
+    // of which there is only one interrupt on the attiny85
     PCMSK |= (1 << PCINT0);
 
     // setup timer TCCR1 at 1Mhz
@@ -44,56 +54,85 @@ int main(void)
     // loop forever, flash pin1 every second to indicate we're
     while (1)
     {
-
-        if (rcpulse <= 1200) {
-            led = BLUE_LED;
+        if (pwm_p0 <= LOW_PWM) {
+            // everything is off if p0 is low
+            PORTB = CAMERA_OFF;
         }
-        else if (rcpulse <= 1800) {
-            led = RED_LED;
+        else {
+            if (pwm_p0 <= MID_PWM) {
+                if (pwm_p1 <= LOW_PWM) {
+                    PORTB = PIC_BURST_ACTION;
+                }
+                else if (pwm_p1 <= MID_PWM) {
+                    PORTB = PICTURE_MODE;
+                }
+                else if (pwm_p1 <= HIGH_PWM) {
+                    PORTB = PIC_TOGGLE_ACTION;
+                }
+                else {
+                    // for now, this is an error condition
+                    PORTB = AUX1_ACTION;
+                }
+            }
+            else if (pwm_p0 <= HIGH_PWM) {
+                if (pwm_p1 <= LOW_PWM) {
+                    PORTB = SHUTTER_ACTION;
+                }
+                else if (pwm_p1 <= MID_PWM) {
+                    PORTB = VIDEO_MODE;
+                }
+                else if (pwm_p1 <= HIGH_PWM) {
+                    PORTB = VID_TOGGLE_ACTION;
+                }
+                else {
+                    // for now, this is an error condition
+                    PORTB = AUX1_ACTION;
+                }
+            }
         }
-        else if (rcpulse <= 2200) {
-            led = YELLOW_LED;
-        }
-        else if (rcpulse > 2200) {
-            led = RED_LED | YELLOW_LED | BLUE_LED;
-        }
-
-        PORTB |= (led);
-        wait_sec(secs_to_flash);
-
-        PORTB &= ~(led);
-        wait_sec(secs_to_flash);
+        _delay_ms(100);
     }
 
     return 0;
 }
 
-void wait_sec(int seconds)
-{
-    int i;
-    for (i = 0; i < seconds; i++)
-        _delay_ms(1000);
-}
-
 ISR(TIM1_OVF_vect)
 {
-    tmprcpulse += 256;
+    tpwm_p0 += 256;
+    tpwm_p1 += 256;
 }
 
 ISR(PCINT0_vect)
 {
+    volatile static int16_t pwm_start_p0 = 0;
+    volatile static int16_t pwm_start_p1 = 0;
+
+    uint8_t changedbits;
     uint8_t current_time = TCNT1;
 
-    if ((PINB & (1 << PB0)) == 1) {
-        // LOW to HIGH pin change
+    changedbits = PINB ^ previouspins;
+    previouspins = PINB;
 
-        // clear pulse timer
-        tmprcpulse = 0;
-        rcpulse_start = current_time;
+    if (changedbits & (1 << PB0)) {
+        if ((PINB & (1 << PB0)) == 1) { // rising
+            // clear pulse timer
+            tpwm_p0 = 0;
+            pwm_start_p0 = current_time;
+        }
+        else { // falling
+            pwm_p0 = tpwm_p0 + (current_time - pwm_start_p0);
+        }
     }
-    else {
-        // HIGH to LOW pin change
-        rcpulse = tmprcpulse + (current_time - rcpulse_start);
+
+    if (changedbits & (1 << PB1)) {
+        if ((PINB & (1 << PB1)) == 1) { // rising
+            // clear pulse timer
+            tpwm_p1 = 0;
+            pwm_start_p1 = current_time;
+        }
+        else { // falling
+            pwm_p1 = tpwm_p1 + (current_time - pwm_start_p1);
+        }
     }
 }
 
